bdea00522401ab00c0d1947d4d075f9bbec26b09
Robin Sveningson <robin@ourstudio.se>
2019-09-26 14:25:29 +0200

    Added CSS.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { IEcomExternalProps } from "./types";
 import EcomStore from './ecom-store';
 
+import '../styles/default.css';
+
 const Ecom = (props: IEcomExternalProps) => {
     return <EcomStore {...props} />;
 }
