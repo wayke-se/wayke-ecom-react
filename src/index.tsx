cea8348bd2a7c609d8239a284fe69d67df335502
Robin Sveningson <robin@ourstudio.se>
2019-09-27 08:31:10 +0200

    Moved assets to own folder, added copy to build step and removed styles from js.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { IEcomExternalProps } from "./types";
 import EcomStore from './ecom-store';
 
-import '../styles/default.css';
-
 const Ecom = (props: IEcomExternalProps) => {
     return <EcomStore {...props} />;
 }
